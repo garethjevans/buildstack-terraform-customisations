b80260e461e5cc452ed6c71d046408cf6c1dae96
Cai Cooper <caicooper82@gmail.com>
2018-03-15 15:25:30 +0000

    Removed conflict name parameter in buildstack certs

--- buildstack-certs.tf
+++ buildstack-certs.tf
@@ -7,7 +7,6 @@ variable "key" {
 }
 
 resource "google_compute_ssl_certificate" "buildstack-cert" {
-  name = "buildstack"
   name_prefix = "${var.env_id}"
   description = "user provided ssl private key / ssl certificate pair"
   private_key = "${var.key}"
