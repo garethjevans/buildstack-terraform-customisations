898cd6d87592dcbc20ee4d4f45376f0bd9fab54e
Cai Cooper <caicooper82@gmail.com>
2018-03-15 11:56:07 +0000

    Terraform script to create buildstack certificate

---
+++ buildstack-certs.tf
@@ -0,0 +1,11 @@
+resource "google_compute_ssl_certificate" "buildstack-cert" {
+  name = "buildstack"
+  name_prefix = "${var.env_id}"
+  description = "user provided ssl private key / ssl certificate pair"
+  private_key = "${var.key}"
+  certificate = "${var.certificate}"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
